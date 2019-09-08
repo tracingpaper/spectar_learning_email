66f6ad999d09ced00894062a4a148789877c5269
Arjun <arjunatlast@gmail.com>
2019-09-08 20:24:22 +0530

    Added local file

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 
 const input = "index.html.template";
 const output = "index.html";
+const local = "index.local.html";
 
 const data = fs.readFileSync(input).toString();
 
@@ -9,5 +10,7 @@ const host_url = 'https://tracingpaper-sl-email-template.netlify.com';
 
 // template replacing
 const processed = data.replace(/\<\%host_url\%\>/g, host_url);
+const localized = data.replace(/\<\%host_url\%\>\//g, '');
 
 fs.writeFileSync(output, Buffer.from(processed));
+fs.writeFileSync(local, Buffer.from(localized));
